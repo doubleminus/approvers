a7248e35fbd98dfe87d9554213fa2e3ce1628953
Nate W <nwisman@zendesk.com>
2014-04-23 15:31:00 -0700

    Attempting to dynamically form approver table

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 (function() {
+  var approverArr;
 
   return {
     events: {
-      'app.activated':'submitTicket'
+      'app.activated':'getInfo'
     },
 
-    // Get the Id of our ticket requester
-    getRequesterId: function() {
-      var id = this.ticket().requester().id();
-      //console.log('Requester ID is ' + id);
+    requests: {
+      fetchApprovers: function(id) {
+        return {
+          //GET /api/v2/users/{id}.json
+          url: '/api/v2/users/' + id + '.json',
+          type:'GET',
+          dataType: 'json'
+        };
+      }
     },
 
     // Display a "Submit for approval" dialogue w/potential approvers
@@ -16,10 +22,29 @@
       this.switchTo('submit');
     },
 
-    // Display a list of current user's approvers and approval progress
-    showApprovers: function() {
-      this.switchTo('approvers');
-    }
+    // Get ticket's current approval status, Id of ticket requester, and approvers
+    getInfo: function() {
+      var approvalStatus = this.ticket().customField("custom_field_23909086");
+      //console.log('Ticket approval status is ' + approvalStatus);
+
+      var id = this.ticket().requester().id();
+      //console.log('Requester ID is ' + id);
+
+      var request = this.ajax('fetchApprovers', id);
+      request.done(this.showApprovers);
+      request.fail(this.showError);
+    },
+
+    showApprovers: function(data) {
+      var approverString = data.user.user_fields.approvers;
+      this.approverArr = new Array(approverString.split(';'));
+
+      this.switchTo('submit', data);
+    },
+
+    showError: function() {
+      this.switchTo('error');
+    },
   };
 
 }());
