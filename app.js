a9cdd98d52e9675ac72164aea61bea90aafeebf3
Nate W <nwisman@zendesk.com>
2014-04-22 17:26:12 -0700

    UI improvements

--- app.js
+++ app.js
@@ -2,9 +2,21 @@
 
   return {
     events: {
-      'app.activated':'showApprovers'
+      'app.activated':'submitTicket'
     },
 
+    // Get the Id of our ticket requester
+    getRequesterId: function() {
+      var id = this.ticket().requester().id();
+      //console.log('Requester ID is ' + id);
+    },
+
+    // Display a "Submit for approval" dialogue w/potential approvers
+    submitTicket: function() {
+      this.switchTo('submit');
+    },
+
+    // Display a list of current user's approvers and approval progress
     showApprovers: function() {
       this.switchTo('approvers');
     }
