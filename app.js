189880cc2ef576de56e06368a6470b1b186bf52a
Nate W <nwisman@zendesk.com>
2014-04-25 16:53:31 -0700

    Working through new approach to gathering approvers from Orgs

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 (function() {
-  var approverArr;
 
   return {
     events: {
@@ -14,6 +13,15 @@
           type:'GET',
           dataType: 'json'
         };
+      },
+
+      fetchApprovalOrgs: function(org_name) {
+        return {
+          //GET /api/v2/search.json?query="type:user organization:Approval Org"
+          url: '/api/v2/search.json?query=type%3Auser%20organization%3A' + org_name,
+          type:'GET',
+          dataType: 'json'
+        };
       }
     },
 
@@ -24,20 +32,36 @@
 
     // Get ticket's current approval status, Id of ticket requester, and approvers
     getInfo: function() {
-      var approvalStatus = this.ticket().customField("custom_field_23909086");
+      var orgName;
+      //var approvalStatus = this.ticket().customField("custom_field_23909086");
       //console.log('Ticket approval status is ' + approvalStatus);
 
       var id = this.ticket().requester().id();
       //console.log('Requester ID is ' + id);
 
-      var request = this.ajax('fetchApprovers', id);
+      var ticket = this.ticket();
+      var tagArray = ticket.tags();
+
+      console.log("tagArray: " + tagArray);
+
+      for (var i=0; i<tagArray.length; i++) {
+        if (tagArray[i].length >= 4 && tagArray[i].substr(0,3) == 'ao_') {
+            orgName  = tagArray[i].slice(4, tagArray[i].length);
+        }
+      }
+
+      orgName = 'approvalorg';
+
+      console.log("ORG NAME: " + orgName);
+
+      var request = this.ajax('fetchApprovalOrgs', orgName);
       request.done(this.showApprovers);
       request.fail(this.showError);
     },
 
     showApprovers: function(data) {
-      var approverString = data.user.user_fields.approvers;
-      this.approverArr = new Array(approverString.split(';'));
+      //var approverString = data.user.user_fields.approvers;
+      //this.approverArr = new Array(approverString.split(';'));
 
       this.switchTo('submit', data);
     },
