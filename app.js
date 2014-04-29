459e952782299143b33c34479af584a28c8ab10a
Nate W <nwisman@zendesk.com>
2014-04-29 15:51:06 -0700

    Improving base functionality prior to submitting for approval

--- app.js
+++ app.js
@@ -1,74 +1,79 @@
 (function() {
 
-  return {
-    events: {
-      'app.activated':'getInfo'
-    },
+    return {
+        events: {
+            'app.activated':'getInfo'
+        },
 
-    requests: {
-      fetchApprovers: function(id) {
-        return {
-          //GET /api/v2/users/{id}.json
-          url: '/api/v2/users/' + id + '.json',
-          type:'GET',
-          dataType: 'json'
-        };
-      },
+        requests: {
+            fetchApprovers: function(id) {
+                return {
+                    //GET /api/v2/users/{id}.json
+                    url: '/api/v2/users/' + id + '.json',
+                    type:'GET',
+                    dataType: 'json'
+                };
+            },
 
-      fetchApprovalOrgs: function(org_name) {
-        return {
-          //GET /api/v2/search.json?query="type:user organization:Approval Org"
-          url: '/api/v2/search.json?query=type%3Auser%20organization%3A' + org_name,
-          type:'GET',
-          dataType: 'json'
-        };
-      }
-    },
+            fetchApprovalOrgs: function(org_name) {
+                return {
+                    //GET /api/v2/search.json?query="type:organization name:<organization name>"
+                    url: '/api/v2/search.json?query=type%3Aorganization%20name%3A' + org_name,
+                    type:'GET',
+                    dataType: 'json'
+                };
+            }
+        },
 
-    // Display a "Submit for approval" dialogue w/potential approvers
-    submitTicket: function() {
-      this.switchTo('submit');
-    },
+        // Display a "Submit for approval" dialogue w/potential approvers
+        submitTicket: function() {
+            this.switchTo('submit');
+        },
 
-    // Get ticket's current approval status, Id of ticket requester, and approvers
-    getInfo: function() {
-      var orgName;
-      //var approvalStatus = this.ticket().customField("custom_field_23909086");
-      //console.log('Ticket approval status is ' + approvalStatus);
+        // Get ticket's current approval status, Id of ticket requester, and approvers
+        getInfo: function() {
+            var orgName;
 
-      var id = this.ticket().requester().id();
-      //console.log('Requester ID is ' + id);
+            var approvalStatus = this.ticket().customField("custom_field_23909086");
+            //console.log('Ticket approval status is ' + approvalStatus);
 
-      var ticket = this.ticket();
-      var tagArray = ticket.tags();
+            requesterId = this.ticket().requester().id();
+            //console.log('Requester ID is ' + requesterId);
+            //console.log('this.currentUser(): ' + this.currentUser().id());
 
-      console.log("tagArray: " + tagArray);
+            // Cannot resubmit for approval if already approved
+            if (approvalStatus == "approved") {
+                this.switchTo('approved');
+            }
+            // Give user option to submit ticket for approval
+            else {
+                var approvalDept = this.ticket().customField("custom_field_23894827");
+                console.log("Approval department (organization): " + approvalDept);
 
-      for (var i=0; i<tagArray.length; i++) {
-        if (tagArray[i].length >= 4 && tagArray[i].substr(0,3) == 'ao_') {
-            orgName  = tagArray[i].slice(4, tagArray[i].length);
-        }
-      }
+                var request = this.ajax('fetchApprovalOrgs', approvalDept);
 
-      orgName = 'approvalorg';
+                // Only allow ticket requester to submit ticket for approval
+                if (requesterId == this.currentUser().id()) {
+                    request.done(this.showApprovers);
+                }
+                else {
+                    request.done(this.showApproversNotRequester);
+                }
 
-      console.log("ORG NAME: " + orgName);
+                request.fail(this.showError);
+            }
+        },
 
-      var request = this.ajax('fetchApprovalOrgs', orgName);
-      request.done(this.showApprovers);
-      request.fail(this.showError);
-    },
+        showApprovers: function(data) {
+            this.switchTo('submit', data);
+        },
 
-    showApprovers: function(data) {
-      //var approverString = data.user.user_fields.approvers;
-      //this.approverArr = new Array(approverString.split(';'));
+        showApproversNotRequester: function(data) {
+            this.switchTo('not_requester', data);
+        },
 
-      this.switchTo('submit', data);
-    },
-
-    showError: function() {
-      this.switchTo('error');
-    },
-  };
-
+        showError: function() {
+            this.switchTo('error');
+        },
+      };
 }());
